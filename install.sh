6a30212390e779b8afd61436b6a52bc151f71ecf
Geert Pingen <geertpingen@gmail.com>
2022-03-17 20:33:22 +0100

    Adds initial install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,61 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "TODO"
+
+function usage() {
+    cat <<USAGE
+
+    Usage: $0 [-r remove]
+
+    Options:
+        -r, --remove:        Remove all packages
+USAGE
+    exit 1
+}
+
+REMOVE_PACKAGES=false
+while [ "$1" != "" ]; do
+    case $1 in
+    -r | --remove)
+        REMOVE_PACKAGES=true
+        ;;
+    -h | --help)
+        usage
+        ;;
+    *)
+        usage
+        exit 1
+        ;;
+    esac
+    shift
+done
+
+export PATH="$PATH:~/.local/bin"
+
+# install pip if not exists
+if ! command -v python3 -m pip &> /dev/null
+then
+    echo "Installing pip"
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    python3 get-pip.py --user
+fi
+
+# install ansible if not exists
+if ! command -v ansible-playbook &> /dev/null
+then
+    echo "Installing ansible with pip"
+    python3 -m pip install --user ansible
+fi
+
+# fetch playbook
+PLAYBOOK_FILE=main.yaml
+if [[ $REMOVE_PACKAGES == false ]]; then
+    PLAYBOOK_FILE=rollback.yaml
+fi
+if [[ test -f "$PLAYBOOK_FILE" == false]]; then
+    echo "Retrieving playbook: $PLAYBOOK_FILE"
+    wget -O ./$PLAYBOOK_FILE https://raw.githubusercontent.com/geertpingen/home/main/ansible/$PLAYBOOK_FILE
+fi
+
+# run playbook
+echo "Running playbook: $PLAYBOOK_FILE"
+ansible-playbook $PLAYBOOK_FILE
