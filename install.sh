0bd00d0ff89ed51514a5790933f6d523d1b40fac
Geert Pingen <geertpingen@gmail.com>
2022-03-17 21:24:45 +0100

    Fix install.sh

--- install.sh
+++ install.sh
@@ -29,31 +29,34 @@ while [ "$1" != "" ]; do
     shift
 done
 
-export PATH="$PATH:~/.local/bin"
+export PATH="$PATH:/usr/bin"
+export PATH="$PATH:$HOME/.local/bin"
 
 # install pip if not exists
-if ! command -v python3 -m pip &> /dev/null
+if ! command -v python3 -m pip >/dev/null 2>&1
 then
     echo "Installing pip"
     curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
     python3 get-pip.py --user
+    rm get-pip.py
 fi
 
 # install ansible if not exists
-if ! command -v ansible-playbook &> /dev/null
+if ! command -v ansible-playbook >/dev/null 2>&1
 then
     echo "Installing ansible with pip"
     python3 -m pip install --user ansible
+    export PATH="$PATH:~/.local/bin"
 fi
 
 # fetch playbook if not exists
 PLAYBOOK_FILE=main.yaml
-if ! $REMOVE_PACKAGES
+if $REMOVE_PACKAGES
 then
     PLAYBOOK_FILE=rollback.yaml
 fi
 
-if ! ls $PLAYBOOK_FILE &> /dev/null
+if ! ls $PLAYBOOK_FILE >/dev/null 2>&1
 then
     echo "Retrieving playbook: $PLAYBOOK_FILE"
     wget -O ./$PLAYBOOK_FILE https://raw.githubusercontent.com/geertpingen/home/main/ansible/$PLAYBOOK_FILE
@@ -62,3 +65,4 @@ fi
 # run playbook
 echo "Running playbook: $PLAYBOOK_FILE"
 ansible-playbook $PLAYBOOK_FILE
+rm $PLAYBOOK_FILE
